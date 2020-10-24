49884c97d47964f50f6cbbb4aa8a72707525eaf7
Cole Patterson <seaborncpatterson@gmail.com>
2020-10-23 21:25:42 -0500

    began user questions

---
+++ MethodExercise/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MethodExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //Name: Cole
+        //Favorite Color: Blue
+        //Favorite Animal: Dog
+        //Favorite Band: Coldplay
+
+            Console.WriteLine("Hello - What is your first name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hi, {userName} What is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine($" {color} is an awesome color. What is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine($"Great! Now, what is your favorite band?");
+            var band = Console.ReadLine();
+
+
+
+        }
+    }
+}
